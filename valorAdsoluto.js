955d10c4c7dfbd6e65390214ca1f74a0366ca434
2191sebasjimenez1110 <126474264+2191sebasjimenez1110@users.noreply.github.com>
2023-02-27 09:22:51 -0500

    Delete valorAdsoluto.js

--- valorAdsoluto.js
+++
@@ -1,9 +0,0 @@
-let num= parseFloat(prompt())
-
-    if(num<0){
-        let mul= num*(-1)
-        console.log("el numero de adsoluto es",mul)
-    }
-    else{
-        console.log("el valor adsoluto es:",num)
-    }
